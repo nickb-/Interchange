9404c06919cef8dec4fb8ae7ce1886757630df52
Nick Burns <nick.dale.burns@gmail.com>
2018-07-16 19:59:10 +1200

    VAE for Bitcoin drafted, looks good

--- CodeBase/Downloader.py
+++ CodeBase/Downloader.py
@@ -16,13 +16,13 @@ import datetime
 
 class Downloader():
     
-    def __init__(self, source):
+    def __init__(self, source, data_directory = '../Data/'):
         # connect the api to source (default = poloniex)
         with open("../Secrets/secrets.json") as f:
             secrets = json.load(f)    
             
         self.api = poloniex(secrets[source]['key'], secrets[source]['secret'])
-        self.data_directory = "../Data/"
+        self.data_directory = data_directory
         self.source = source
         
     def parse_date(self, datestamp, output = "utc", format = "%Y-%m-%d %H:%M:%S"):
@@ -37,7 +37,7 @@ class Downloader():
         elif output == "utc":
             if type(datestamp) == datetime.datetime:
                 return datestamp.strftime(format)
-            elif type == float or type == int:    # i.e. unix timestamp
+            elif type(datestamp) == float or type(datestamp) == int:    # i.e. unix timestamp
                 return datetime.datetime.utcfromtimestamp(datestamp).strftime(format)
             else:
                 print("Invalid datestamp")
@@ -46,32 +46,33 @@ class Downloader():
         
     def download(self, method = 'refresh', date_range = {'start_date': '2018-01-01 00:00:00',
                                                          'end_date': '2018-01-01 01:00:00'}):
+        
+        tickers = self.api.returnTicker()
+        destination = "{}/{}".format(self.data_directory, self.source)
+        
         if method == 'refresh':
-            # Download all available tickers from start of 2018 to now
-            date_range['end_date'] = self.parse_date(datetime.datetime.now(), output = 'utc')
-            destination = "{}/{}".format(self.data_directory, self.source)
-            tickers = self.api.returnTicker()
             
-            # check if Source data directory already exists
+            # check if Source data directory already exists, wipe it and recreate it
             if os.path.exists(destination):
                 for existing in os.listdir(destination):
                     os.remove("{}/{}".format(destination, existing))
             else:
                 os.mkdir(destination)
-                
-            # Now, iterate over the tickers and save to CSV files
-            for currency in tickers.keys():
+             
+        for currency in tickers.keys():
+            filename = "PoloniexChartData_{}.csv".format(currency)
+            file_location = "{}/{}".format(destination, filename)
+            
+            # download the data if it does not already exist
+            if not os.path.exists(file_location):
                 data = self.api.returnChartData(currency, date_range['start_date'], date_range['end_date'])
                 data = pd.io.json.json_normalize(data)
-                
-                filename = "PoloniexChartData_{}.csv".format(currency)
-                data.to_csv("{}/{}".format(destination, filename), index = False)
-                
-                
-        else:
-            pass
 
-        
+                data['Datestamp'] = [datetime.datetime.utcfromtimestamp(x).strftime(format = "%Y-%m-%d %H:%M:%S") \
+                                         for x in data['date']]
+                filename = "PoloniexChartData_{}.csv".format(currency)
+                data.to_csv(file_location, index = False)
+                   
 if __name__ == '__main__':
     
     dw = Downloader('poloniex')
